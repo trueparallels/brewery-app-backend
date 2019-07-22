85e4d04edbca31219569596ca4eb5e6bde5fd276
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-07-22 18:28:43 -0400

    fixing tag syntax

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build . -t brewery-app-backend:latest
+docker build . -t $ECR_URL:latest
 
-IMAGE_ID=$(docker images --filter=reference='brewery-app-backend:latest' --format "{{.ID}}")
+# IMAGE_ID=$(docker images --filter=reference='brewery-app-backend:latest' --format "{{.ID}}")
 
-docker tag $IMAGE_ID $ECR_URL:build-$CIRCLE_BUILD_NUM $ECR_URL:latest
+docker tag $ECR_URL:build-$CIRCLE_BUILD_NUM $ECR_URL:latest
 docker push $ECR_URL:latest
