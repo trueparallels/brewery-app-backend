c426a20e139dee8ba203481884ad4d7add847975
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-07-22 18:32:21 -0400

    push build number build

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker build . -t $ECR_URL:latest
-
-# IMAGE_ID=$(docker images --filter=reference='brewery-app-backend:latest' --format "{{.ID}}")
+docker build . -t $ECR_URL:build-$CIRCLE_BUILD_NUM
+docker push $ECR_URL:build-$CIRCLE_BUILD_NUM
 
 docker tag $ECR_URL:build-$CIRCLE_BUILD_NUM $ECR_URL:latest
 docker push $ECR_URL:latest
