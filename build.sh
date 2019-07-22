33905b992ea71006442ac700c684ea554ec6180d
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-07-22 18:25:41 -0400

    fixing my bash fail

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build . -t brewery-app-backend:latest
 
-IMAGE_ID = $(docker images --filter=reference='brewery-app-backend:latest' --format "{{.ID}}")
+IMAGE_ID=$(docker images --filter=reference='brewery-app-backend:latest' --format "{{.ID}}")
 
 docker tag $IMAGE_ID $ECR_URL:build-$CIRCLE_BUILD_NUM $ECR_URL:latest
 docker push $ECR_URL:latest
